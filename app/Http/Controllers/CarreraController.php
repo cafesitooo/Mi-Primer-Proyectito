5759712dbb3cfc5e47473640c6f3392b12aa1ba5
Matias Leila <matias.leila@alumnos.ucn.cl>
2021-09-26 19:51:21 -0300

    cambios de la tercera ayudantia solo falta el html de view carreras

---
+++ app/Http/Controllers/CarreraController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Carrera;
+use Illuminate\Http\Request;
+
+class CarreraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $carreras = Carrera::all();  //Lo que realiza es llamar de la base de datos todas las carreras
+        return view('carreras.index')->with('carrera',$carreras); //Muestra todas las carreras de la base de datos
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Carrera  $carrera
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Carrera $carrera)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Carrera  $carrera
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Carrera $carrera)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Carrera  $carrera
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Carrera $carrera)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Carrera  $carrera
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Carrera $carrera)
+    {
+        //
+    }
+}
